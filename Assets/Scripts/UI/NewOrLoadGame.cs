92224ca9abf00f7546e1fe0a56a5f3997f19b0cf
MibuWolf <sanmuwangzi@126.com>
2017-02-21 20:32:34 +0800

    NGUI PlayeSound 脚本为按钮添加音效 向主相机增加背景音乐 为新游戏 加载游戏添加回调记录游戏是否为新游戏

---
+++ Assets/Scripts/UI/NewOrLoadGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewOrLoadGame : MonoBehaviour {
+
+    public void onNewGame()
+    {
+        PlayerPrefs.SetInt("isNewGame", 1);
+
+        print("onClick NewGame");
+    }
+
+
+    public void onLoadGame()
+    {
+        PlayerPrefs.SetInt("isNewGame", 1);
+
+        print("onClick LoadGame");
+    }
+}
