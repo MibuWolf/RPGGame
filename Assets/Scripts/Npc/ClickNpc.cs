6af1bb1adab840e70cf0426538699701ee3a1280
MibuWolf <sanmuwangzi@126.com>
2017-03-06 19:18:23 +0800

    点击NPC界面接受任务界面变化

--- Assets/Scripts/Npc/ClickNpc.cs
+++ Assets/Scripts/Npc/ClickNpc.cs
@@ -6,6 +6,13 @@ public class ClickNpc : MonoBehaviour {
 
     public TweenPosition tween;
 
+    public UILabel lbQuest;
+    private int nCount = 0;
+
+    public GameObject btnOK;
+    public GameObject btnAccept;
+    public GameObject btnCancel;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -28,7 +35,24 @@ public class ClickNpc : MonoBehaviour {
         tween.PlayForward();
     }
 
+    // 关闭按钮
     public void onClickCloseQuest()
+    {
+        tween.PlayReverse();
+    }
+
+    // 点击接受按钮
+    public void onClickAccept()
+    {
+        lbQuest.text = "任务:\n \n 你已经杀死了" + nCount+" \\ 10 只小野狼。 \n \n 奖励: 1000金币。";
+
+        btnOK.SetActive(true);
+        btnAccept.SetActive(false);
+        btnCancel.SetActive(false);
+    }
+
+    // 点击取消按钮
+    public void onClickCancel()
     {
         tween.PlayReverse();
     }
